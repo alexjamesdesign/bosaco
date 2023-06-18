3dea6093242622b226f669a4bf034afd2d19918f
Alex James <hello@alexjamesdesign.co.uk>
2023-06-18 23:37:11 +0100

    progress with building the contact template

--- wp-content/themes/bosaco/page.php
+++ wp-content/themes/bosaco/page.php
@@ -3,5 +3,7 @@
 $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
+$context['posts'] = new Timber\PostQuery();
+
 
 Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page.twig' ], $context );

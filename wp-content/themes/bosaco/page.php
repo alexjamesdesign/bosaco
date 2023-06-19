9b7efe3e8c84c07eef11864ec25f1823629bb7bd
Alex James <hello@alexjamesdesign.co.uk>
2023-06-19 12:56:52 +0100

    Bug testing featured image on contact

--- wp-content/themes/bosaco/page.php
+++ wp-content/themes/bosaco/page.php
@@ -5,5 +5,7 @@ $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 $context['posts'] = new Timber\PostQuery();
 
+$context['featured_image'] = get_the_post_thumbnail_url(852);
+
 
 Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page.twig' ], $context );

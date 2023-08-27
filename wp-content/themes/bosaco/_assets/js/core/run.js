cddb8299f093a22deb154584016ec74237d13ded
Alex James <hello@alexjamesdesign.co.uk>
2023-08-27 17:57:08 +0100

    Added transitions and fixed issue with links

--- wp-content/themes/bosaco/_assets/js/core/run.js
+++ wp-content/themes/bosaco/_assets/js/core/run.js
@@ -5,6 +5,25 @@
 	// add all of your code within here, not above or below
 	$(function () {
 
+		// Smaller header
+		$(window).scroll(function() {
+			if ($(document).scrollTop() > 150) {
+			  $('.nt-sticky').addClass('nt-shrink');
+			} else {
+			  $('.nt-sticky').removeClass('nt-shrink');
+			}
+		});
+
+		var accordionContent = $('accordion-content');
+
+		if ($(window).width() > 1000) {
+			$('.accordion-content').slideDown();
+		}
+
+		$('.accordion-header').click(function() {
+			$(this).siblings('.accordion-content').slideToggle();
+		});
+
 
 	});
 
--- wp-content/themes/bosaco/index.php
+++ wp-content/themes/bosaco/index.php
@@ -3,9 +3,13 @@
 $context = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 
-$context['categories'] = get_categories( array(
-    'orderby' => 'name',
-    'order'   => 'ASC'
-) );
+// $context['categories'] = get_categories( array(
+//     'orderby' => 'name',
+//     'order'   => 'ASC'
+// ) );
+
+$context['post'] = $timber_post; 
+
+$context['categories'] = Timber::get_terms($args);
 
 Timber::render( [ 'index.twig' ], $context );

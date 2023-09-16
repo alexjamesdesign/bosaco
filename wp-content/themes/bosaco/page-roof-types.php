adb241f0ce9cee15bf5e042986ec001aa257df06
Alex James <hello@alexjamesdesign.co.uk>
2023-09-16 21:31:06 +0100

    Template created for the roof types page

---
+++ wp-content/themes/bosaco/page-roof-types.php
@@ -0,0 +1,13 @@
+<?php
+
+// Template Name: Roof Types
+
+$context = Timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+$context['posts'] = new Timber\PostQuery();
+
+$context['featured_image'] = get_the_post_thumbnail_url(852);
+
+
+Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page-roof-types.twig' ], $context );
